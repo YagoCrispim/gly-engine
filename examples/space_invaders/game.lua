42c03b947e1b59a9288a9531214026c1415d8de2
Yago Crispim <34103098+YagoCrispim@users.noreply.github.com>
2024-10-05 18:43:13 -0300

    feat: impl alien group

--- examples/space_invaders/game.lua
+++ examples/space_invaders/game.lua
@@ -3,53 +3,6 @@ local os = require('os')
 ---@type Timeout[]
 local timers = {}
 
----@param time_in_seconds number
----@param cb function
-local function set_timeout(time_in_seconds, cb)
-    local timer = os.time() + time_in_seconds
-    table.insert(timers, {time = timer, cb = cb})
-end
-
----@param pos { x: number, y: number }
----@return Alien
-local function Alien(pos)
-    local allow_update = false
-
-    local update_timer
-    update_timer = function()
-        allow_update = true
-        set_timeout(1, update_timer)
-    end
-    update_timer()
-
-    --
-    return {
-        --
-        speed = 40,
-        dim = {w = 20, h = 10},
-        transform = {
-            pos = {
-                --
-                x = pos.x or 0,
-                y = pos.y or 0
-            }
-        },
-
-        update = function(alien)
-            if not allow_update then return end
-
-            alien.transform.pos.x = alien.transform.pos.x + alien.speed
-            allow_update = false
-        end,
-
-        draw = function(alien, std, _)
-            std.draw.color(std.color.white)
-            std.draw.rect(0, alien.transform.pos.x, alien.transform.pos.y,
-                          alien.dim.w, alien.dim.h)
-        end
-    }
-end
-
 ---@param x number
 ---@param y number
 ---@return Vector2D
@@ -62,11 +15,92 @@ local function Vector2D(x, y)
             self.x = self.x + v
             self.y = self.y + v
             return self
+        end,
+
+        copy = function(self) return Vector2D(self.x, self.y) end
+    }
+end
+
+---@param time_in_seconds number
+---@param cb function
+local function set_timeout(time_in_seconds, cb)
+    local timer = os.time() + time_in_seconds
+    table.insert(timers, {time = timer, cb = cb})
+end
+
+---@param pos { x: number, y: number }
+---@return Alien
+local function Alien(pos)
+    return {
+        --
+        dim = {w = 20, h = 10},
+        transform = {pos = Vector2D(pos.x, pos.y)},
+
+        update = function(alien)
+            -- alien.transform.pos.x = alien.transform.pos.x + alien.speed
+            -- allow_update = false
+        end,
+
+        draw = function(alien, std, _)
+            std.draw.color(std.color.white)
+            std.draw.rect(0, alien.transform.pos.x, alien.transform.pos.y,
+                          alien.dim.w, alien.dim.h)
         end
     }
 end
 
-local function Group()end
+---@param nodes Alien[]
+---@param columns number
+---@return AlienGroup
+local function AlienGroup(nodes, columns)
+    local allow_update = false
+
+    local update_timer
+    update_timer = function()
+        allow_update = true
+        set_timeout(1, update_timer)
+    end
+    update_timer()
+
+    ---@type AlienGroup
+    return {
+        nodes = nodes or {},
+        speed = 50,
+        dim = {w = 20, h = 10},
+        transform = {pos = Vector2D(0, 0)},
+        limits = {
+            r = nodes[1].transform.pos.x,
+            l = (nodes[1].transform.pos.x + nodes[1].dim.w) * columns
+        },
+
+        update = function(self)
+            if not allow_update then return end
+
+            local new_pos = self.transform.pos:copy()
+            new_pos.x = new_pos.x + self.speed
+            self:move(new_pos)
+
+            allow_update = false
+        end,
+
+        draw = function(self, std, game)
+            for _, v in pairs(self.nodes) do v:draw(std, game) end
+        end,
+
+        move = function(self, new_pos)
+            local pos = self.transform.pos
+            local xdiff = pos.x - new_pos.x
+            local ydiff = pos.y - new_pos.y
+
+            for _, v in pairs(self.nodes) do
+                v.transform.pos.x = v.transform.pos.x - xdiff
+                v.transform.pos.y = v.transform.pos.y - ydiff
+            end
+
+            self.transform.pos = new_pos
+        end
+    }
+end
 
 return {
     meta = {
@@ -85,11 +119,13 @@ return {
             local alien_w = 20
             local spacing = 40
 
+            local objs = {}
+
             game._state = {rlimit = 10, llimit = pos.x + alien_w + aliens.col}
 
             while processed.line <= aliens.lines do
                 while processed.col <= aliens.col do
-                    table.insert(game._go, Alien(Vector2D(pos.x, pos.y)))
+                    table.insert(objs, Alien(Vector2D(pos.x, pos.y)))
                     pos.x = pos.x + spacing
                     processed.col = processed.col + 1
                 end
@@ -98,6 +134,9 @@ return {
                 processed.line = processed.line + 1
                 processed.col = 0
             end
+
+            local aliens_group = AlienGroup(objs, 10)
+            table.insert(game._go, aliens_group)
         end,
 
         loop = function(_, game)
@@ -140,6 +179,7 @@ return {
 ---@field x number
 ---@field y number
 ---@field sum fun(self: Vector2D, v: number): Vector2D
+---@field copy fun(self: Vector2D): Vector2D
 --
 ---@class Alien
 ---@field speef number
@@ -151,3 +191,12 @@ return {
 ---@class Timeout
 ---@field time number
 ---@field cb { time: number, cb: function }
+--
+---@class AlienGroup
+---@field nodes Alien[]
+---@field speed number
+---@field dim { w: number, h: number }
+---@field transform { pos: Vector2D }
+---@field update fun(self: AlienGroup): nil
+---@field draw fun(self: AlienGroup, std: table, game: table): nil
+---@field move fun(self: AlienGroup, new_pos: Vector2D): nil
